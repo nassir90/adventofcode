9f1c944db56b5d86dfbdb559a2bed01395d77ec4
Naza <nozolo90@gmail.com>
2021-12-13 06:19:50 +0000

    Made some compatibility adjustments

--- starter.py
+++ starter.py
@@ -1,8 +1,8 @@
 import sys
 
-def get_puzzle_input(day: int):
+def get_puzzle_input(day: int, old=True):
     if len(sys.argv) == 2 and sys.argv[1] == "r":
-        file = open("input%d" % day)
+        file = open(("input%d" % day) if old else "input")
     else:
-        file = open("example%d" % day)
-    return [ line for line in file ]
+        file = open(("example%d" % day) if old else "example")
+    return [ line.strip() for line in file ]
