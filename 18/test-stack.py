a65948b8a40dd7dcc9483d3a1534f01b30cb804e
Naza <nozolo90@gmail.com>
2021-12-18 09:26:53 +0000

    Test code for finding rightmost number is working, the rest should be easy

---
+++ 18/test-stack.py
@@ -0,0 +1,48 @@
+test =  [[[[[4,3],4],4],[69,[[2,4],3]]],[1,1]]
+
+xs = [0]
+ps = [test]
+
+while xs:
+    if xs[-1] <= 1:
+        if isinstance(ps[-1][xs[-1]], int):
+            if ps[-1][xs[-1]] == 69:
+                # Find the right integer
+                psc = ps.copy()
+                xsc = xs.copy()
+
+                while xsc and xsc[-1] == 1:
+                    psc.pop()
+                    xsc.pop()
+                if xsc:
+                    xsc[-1] = 1
+                while xsc:
+                    if xsc[-1] <= 1:
+                        if isinstance(psc[-1][xsc[-1]], int):
+                            print("The number right of 69 is %d" % psc[-1][xsc[-1]])
+                            break
+                        else:
+                            psc.append(psc[-1][xsc[-1]])
+                            xsc.append(0)
+                    else:
+                        xsc.pop()
+                        psc.pop()
+                        if xsc:
+                            xsc[-1] += 1
+
+            xs[-1] += 1
+        else:
+            if len(xs) == 4:
+                #ps[-1][xs[-1]] = 0
+                #xs = [0]
+                #ps = [test]
+                xs[-1] += 1
+            else:
+                ps.append(ps[-1][xs[-1]])
+                xs.append(0)
+    else:
+        xs.pop()
+        ps.pop()
+        if xs:
+            xs[-1] += 1
+
