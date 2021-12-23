6b2e0d4ef117d670d8f49b678b0ea38f92017ff1
Naza <nozolo90@gmail.com>
2021-12-23 00:01:42 +0000

    Added working solution for day 22

--- 22/2.py
+++ 22/2.py
@@ -8,25 +8,12 @@ for line in get_puzzle_input(22, False):
     coordinates = cuboid.split(",")
     ranges = []
     for coordinate in coordinates:
-        ranges.append([int(point) for point in coordinate[2:].split("..")])
+        r = [int(point) for point in coordinate[2:].split("..")]
+        r[1] += 1
+        ranges.append(r)
     commands.append([command, ranges])
 
-on = [ [ [False for z in range(-50,51) ] for y in range(-50, 51) ] for x in range(-50, 51) ]
-first_non_init = 0
-
-area = 0
-
 ranges = []
-
-def point_in_range(point, r):
-#    print(point)
-#    print(r)
-    (xmin, xmax), (ymin, ymax), (zmin, zmax) = r
-    x, y, z = point
-    return x >= xmin and x < xmax and \
-        y >= ymin and y < ymax and \
-        z >= zmin and z < zmax
-
 
 def clamp(number, minimum, maximum):
     return max(minimum, min(number, maximum))
@@ -34,28 +21,13 @@ def clamp(number, minimum, maximum):
 def determine_point_overlaps_of(obj, subject):
     (xmin1, xmax1), (ymin1, ymax1), (zmin1, zmax1) = obj
     (xmin2, xmax2), (ymin2, ymax2), (zmin2, zmax2) = subject
-    xmin1 = clamp(xmin1, xmin2, xmax2-1)
-    xmax1 = clamp(xmax1, xmin2, xmax2-1)
-    ymin1 = clamp(ymin1, ymin2, ymax2-1)
-    ymax1 = clamp(ymax1, ymin2, ymax2-1)
-    zmin1 = clamp(zmin1, zmin2, zmax2-1)
-    zmax1 = clamp(zmax1, zmin2, zmax2-1)
-    overlaps = []
-    points = [
-        (xmin1, ymin1, zmin1),
-        (xmin1, ymin1, zmax1),
-        (xmin1, ymax1, zmin1),
-        (xmin1, ymax1, zmax1),
-        (xmax1, ymin1, zmin1),
-        (xmax1, ymin1, zmax1),
-        (xmax1, ymax1, zmin1),
-        (xmax1, ymax1, zmax1)
-    ]
-    for point in points:
-        if point_in_range(point, subject):
-            overlaps += [point]
-    print(len(overlaps))
-    return overlaps
+    xmin1 = clamp(xmin1, xmin2, xmax2)
+    xmax1 = clamp(xmax1, xmin2, xmax2)
+    ymin1 = clamp(ymin1, ymin2, ymax2)
+    ymax1 = clamp(ymax1, ymin2, ymax2)
+    zmin1 = clamp(zmin1, zmin2, zmax2)
+    zmax1 = clamp(zmax1, zmin2, zmax2)
+    return (xmin1, xmax1), (ymin1, ymax1), (zmin1, zmax1)
 
 def get_area(r):
     return abs((r[0][1] - r[0][0]) * (r[1][1] - r[1][0]) * (r[2][1] - r[2][0]))
@@ -63,12 +35,23 @@ def get_area(r):
 # In order to add a range, check for overlaps, delete any overlaps (union) and then simply add the range to the ranges array
 def delete(r):
     new_ranges = []
-    (xmin, xmax), (ymin, ymax), (zmin, zmax) = r
     for r2 in ranges:
-        (xmin2, xmax2), (ymin2, ymax2), (zmin2, zmax2) = r2
-        overlaps = determine_point_overlaps_of(r, r2)
-        if len(overlaps) == 8:
-            new_ranges.append(r2)
+        (xmin2, xmax2), (ymin2, ymax2), (zmin2, zmax2) = r2 
+        (xmin, xmax), (ymin, ymax), (zmin, zmax) = determine_point_overlaps_of(r, r2)
+
+        top = [(xmin2, xmax2), (ymin2, ymax2), (zmax, zmax2)]
+        bottom = [(xmin2, xmax2), (ymin2, ymax2), (zmin2, zmin)]
+        left = [(xmin2, xmin), (ymin2, ymax2), (zmin, zmax)]
+        right = [(xmax, xmax2), (ymin2, ymax2), (zmin, zmax)]
+        front = [(xmin, xmax), (ymin2, ymin), (zmin, zmax)]
+        back = [(xmin, xmax), (ymax, ymax2), (zmin, zmax)]
+
+        parts = (top, bottom, left, right, front, back)
+
+        if sum(get_area(part) for part in parts) == get_area(r2):
+            new_ranges += [ r2 ] 
+        else:
+            new_ranges += parts
     return [ r for r in new_ranges if get_area(r) != 0 ]
 
 def add(r):
@@ -79,7 +62,7 @@ def add(r):
 def calculate_area():
     area = 0
     for (lx, hx), (ly, hy), (lz, hz) in ranges:
-        area += abs((hx - lx) * ( hy - ly) * (hz - lz))
+        area += abs((hx - lx) * (hy - ly) * (hz - lz))
     return area
 
 for command in commands:
@@ -87,8 +70,4 @@ for command in commands:
         ranges = add(command[1])
     elif command[0] == "off":
         ranges = delete(command[1])
-    for r in ranges:
-        print(r)
-    print("Area is now:", calculate_area())
-
-# Do final block
+    print(calculate_area())
