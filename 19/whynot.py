82c590d9b712c04793855c4e10b55066e6435dd9
Naza <uzoukwuc@tcd.ie>
2022-09-29 09:27:49 +0100

    Updated to use ISO terminiloty

--- 19/whynot.py
+++ 19/whynot.py
@@ -4,11 +4,10 @@ import rotations
 # this array returns the positions of the points in the rectifend
 # relative to those in the rectaum. If the arrays cannot be rectified,
 # this function returns none
-
 scanner_positions = []
 
-def rectify(rectifend, rectaum):
-    map_list = list(rectifend)
+def rectify(map, beacons):
+    map_list = list(map)
     map_clone = [list(point) for point in map_list]
     for i, map_anchor in enumerate(map_clone):
         map_anchor_clone = map_anchor.copy()
@@ -16,37 +15,36 @@ def rectify(rectifend, rectaum):
             point[0] -= map_anchor_clone[0]
             point[1] -= map_anchor_clone[1]
             point[2] -= map_anchor_clone[2]
-        for center_of_rotation in rectaum:
+        for center_of_rotation in beacons:
             center_of_rotation_clone = center_of_rotation.copy()
-            for point in rectaum:
+            for point in beacons:
                 point[0] -= center_of_rotation_clone[0]
                 point[1] -= center_of_rotation_clone[1]
                 point[2] -= center_of_rotation_clone[2]
             assert tuple(center_of_rotation) == tuple(map_anchor) == (0,0,0)
-            rectified, r = rotation_where_overlap_occurs(map_clone, rectaum)
-            if rectified:
+            rotation = rotation_where_overlap_occurs(map_clone, beacons)
+            if rotation:
                 beacons = [(point[0]+map_list[i][0],
                             point[1]+map_list[i][1],
-                            point[2]+map_list[i][2]) for point in rectified]
+                            point[2]+map_list[i][2]) for point in rotation]
                 scanner_position = beacons.pop()
                 return beacons, scanner_position
     return None, None
 
 # Takes an array of beacons and for each of the 24 relevant rotations,
 # runs predicate on it.
-def rotation_where_overlap_occurs(rectifend, beacons):
+def rotation_where_overlap_occurs(map, beacons):
     for i, rotation in enumerate(zip(*tuple(rotations.get_rotations(point) for point in beacons))):
-        rectifend_set = set(tuple(tuple(point) for point in rectifend))
-        intersection = len(rectifend_set.intersection(rotation[:-1]))
+        map_set = set(tuple(tuple(point) for point in map))
+        intersection = len(map_set.intersection(rotation[:-1]))
         if intersection >= share_threshold:
-            return rotation, i
-    return None, None
+            return rotation
 
 detected_beacons_per_scanner = []
 detected_beacons = []
 share_threshold = 12
 
-for line in open("input"):
+for line in open("example"):
     beacon_points = line.split(",")
     if len(beacon_points) == 1:
         detected_beacons.append([0,0,0]) # Put the scanner center at the end
